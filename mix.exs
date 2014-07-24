b324d1d666f926fbeff207eb300bb5103cb3080b
Frank Liu <gniquil@gmail.com>
2014-07-23 19:23:12 -0700

    added license and some info for publishing with hex

--- mix.exs
+++ mix.exs
@@ -2,30 +2,26 @@ defmodule SweetXml.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :sweet_xml,
-     version: "0.1.0",
-     elixir: "~> 0.14.2",
-     name: "SweetXml",
-     source_url: "https://github.com/gniquil/sweet_xml",
-     deps: deps]
+    [
+      app: :sweet_xml,
+      version: "0.1.0",
+      elixir: "~> 0.14.2",
+      deps: deps,
+      package: [
+        contributors: ["Frank Liu"]
+        licenses: ["MIT"],
+        links: ["https://github.com/gniquil/sweet_xml"],
+      ],
+      description: """
+      An sweet wrapper of :xmerl to help query xml docs
+      """
+    ]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type `mix help compile.app` for more information
   def application do
-    [applications: []]
+    []
   end
 
-  # Dependencies can be hex.pm packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1"}
-  #
-  # Type `mix help deps` for more examples and options
   defp deps do
     []
   end
