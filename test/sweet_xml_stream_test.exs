f8480f423c5b2799060e7747718c51bf044bfc15
Frank Liu <gniquil@gmail.com>
2015-04-12 12:53:58 -0700

    added test for handling xmerl_options

--- test/sweet_xml_stream_test.exs
+++ test/sweet_xml_stream_test.exs
@@ -32,6 +32,18 @@ defmodule SweetXmlStreamTest do
     assert result == ['Nested Head', 'XML Parsing']
   end
 
+  test "stream tags with xmerl_options", %{simple_stream: simple_stream} do
+    result = simple_stream
+    |> stream_tags([:li, :special_match_key], discard: [:li, :special_match_key], space: :normalize)
+    |> Stream.map(fn
+        {_, doc} ->
+          xpath(doc, ~x"./text()")
+      end)
+    |> Enum.to_list
+
+    assert result == [' First', 'Second ', 'Third', 'Forth', 'first star']
+  end
+
   test "tag both given and discarded", %{simple_stream: simple_stream} do
     result = simple_stream
     |> stream_tags(:head, discard: [:head])
